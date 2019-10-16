c51fa9d08ea07bc215486259b4a7c02d1b18941b
justynabierylo <justyna.bierylo@gmail.com>
2019-10-16 22:52:50 +0200

    progress commit - webpack (boostrap and jquery)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
 import './styles/main.scss';
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
+import 'bootstrap';
+// import TweenMax from 'TweenMax';
+import ScrollMagic from 'ScrollMagic';
+// import 'animation.gsap';
+// import 'debug.addIndicators';
+
+// init controller
+var controller = new ScrollMagic.Controller();
+
+// build scene
+var scene = new ScrollMagic.Scene({triggerElement: ".dynamicContent #loader", triggerHook: "onEnter"})
+                .addTo(controller)
+                .on("enter", function (e) {
+                    if (!$("#loader").hasClass("active")) {
+                        $("#loader").addClass("active");
+                        if (console){
+                            console.log("loading new items");
+                        }
+                        // simulate ajax call to add content using the function below
+                        setTimeout(addBoxes, 1000, 9);
+                    }
+                });
+
+// pseudo function to add new content. In real life it would be done through an ajax request.
+function addBoxes (amount) {
+    for (var i=1; i<=amount; i++) {
+        $("<div></div>")
+            .addClass("box1")
+            .appendTo("#content");
+    }
+    // "loading" done -> revert to normal state
+    scene.update(); // make sure the scene gets the new start position
+    $("#loader").removeClass("active");
+}
+
+// add some boxes to start with.
+addBoxes(30);
+
+// console.log(ScrollMagic)
--- webpack.config.js
+++ webpack.config.js
@@ -46,6 +46,18 @@ module.exports = {
             }
         ]
     },
+    resolve: {
+        modules: ['node_modules'],
+        alias: {
+          'TweenLite': 'gsap/src/minified/TweenLite.min.js',
+          'TweenMax': 'gsap/src/minified/TweenMax.min.js',
+          'TimelineLite': 'gsap/src/minified/TimelineLite.min.js',
+          'TimelineMax': 'gsap/src/minified/TimelineMax.min.js',
+          'ScrollMagic': 'scrollmagic/scrollmagic/minified/ScrollMagic.min.js',
+          'animation.gsap': 'scrollmagic/scrollmagic/minified/plugins/animation.gsap.min.js',
+          'debug.addIndicators': 'scrollmagic/scrollmagic/minified/plugins/debug.addIndicators.min.js'
+        }
+    },
     plugins: [
         new HtmlWebPackPlugin({
             template: "./src/index.html",
@@ -54,6 +66,10 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: "[name].css",
             chunkFilename: "[id].css"
+        }),
+        new webpack.ProvidePlugin({
+            $: "jquery",  
+            jQuery: "jquery" 
         }),
         new webpack.HotModuleReplacementPlugin()
     ]
